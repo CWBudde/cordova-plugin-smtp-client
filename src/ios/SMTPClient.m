528fc73e85fbcfede243ff7a2bab8ca449670ed0
Christian <Christian@savioursofsoul.de>
2017-10-05 02:04:21 +0200

    Some basic formatting

--- src/ios/SMTPClient.m
+++ src/ios/SMTPClient.m
@@ -6,153 +6,164 @@
 
 - (void)cordovaSendMail:(CDVInvokedUrlCommand*)command
 {
-   	self.pluginCallbackId = command.callbackId;
+	self.pluginCallbackId = command.callbackId;
  
-   	NSString *args = [command.arguments objectAtIndex:0];
-    NSData *objectData = [args dataUsingEncoding:NSUTF8StringEncoding];
-    NSError *jsonError;
-    NSDictionary *json = [
-       	NSJSONSerialization JSONObjectWithData:objectData
-   	   	options:NSJSONReadingMutableContainers
-   	   	error:&jsonError
-   	];
+	NSString *args = [command.arguments objectAtIndex:0];
+	NSData *objectData = [args dataUsingEncoding:NSUTF8StringEncoding];
+	NSError *jsonError;
+	NSDictionary *json = [
+		NSJSONSerialization JSONObjectWithData:objectData
+		options:NSJSONReadingMutableContainers
+		error:&jsonError
+	];
 
-    NSString *from = [json objectForKey:@"emailFrom"];
-    NSString *to = [json objectForKey:@"emailTo"];
-    NSString *bcc = [json objectForKey:@"bcc"];
-    NSString *smtpServer = [json objectForKey:@"smtp"];
-    NSString *smtpUser = [json objectForKey:@"smtpUserName"];
-    NSString *smtpPassword = [json objectForKey:@"smtpPassword"];
-    NSString *textBody = [json objectForKey:@"textBody"];
-    
-    SKPSMTPMessage *message = [[SKPSMTPMessage alloc] init];
-    
-    message.fromEmail = from;
-    message.toEmail = to;
-    if (bcc == (id)[NSNull null] || bcc.length == 0 ){
-		    message.bccEmail = nil;
-    } else {
-		    message.bccEmail = bcc;		
-    }
-
-    message.relayHost = smtpServer;
-    //message.requiresAuth = [json objectForKey:@"smtpRequiresAuth"];
-    message.requiresAuth = true;
-    
-    if (message.requiresAuth) {
-        message.login = smtpUser;
-        message.pass = smtpPassword;
-    }
-    
-    message.wantsSecure = true; // smtp.gmail.com doesn't work without TLS!
-    
-    message.subject = [json objectForKey:@"subject"];
-    
-    // Only do this for self-signed certs, test only
-    // testMsg.validateSSLChain = NO;
-
-    message.delegate = self;
-    
-    NSDictionary *plainPart = [NSDictionary dictionaryWithObjectsAndKeys:@"text/plain; charset=UTF-8", kSKPSMTPPartContentTypeKey,
-                               textBody, kSKPSMTPPartMessageKey, @"8bit", kSKPSMTPPartContentTransferEncodingKey, nil];
-    
-    NSMutableArray *partsToSend = [NSMutableArray arrayWithObjects:plainPart, nil];
-    
-    NSArray *files = [json objectForKey:@"attachmentsInBase64Format"];
-
-    NSError *error = nil;
-	
-    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\/(.*)\;" options:NSRegularExpressionCaseInsensitive error:&error];
-
-    
-    for (NSString *file in files) {
+	NSString *from = [json objectForKey:@"emailFrom"];
+	NSString *to = [json objectForKey:@"emailTo"];
+	NSString *bcc = [json objectForKey:@"bcc"];
+	NSString *smtpServer = [json objectForKey:@"smtp"];
+	NSString *smtpUser = [json objectForKey:@"smtpUserName"];
+	NSString *smtpPassword = [json objectForKey:@"smtpPassword"];
+	NSString *textBody = [json objectForKey:@"textBody"];
 		
-       	NSArray* imageDataSplitByComma = [file componentsSeparatedByString:@","];
+	SKPSMTPMessage *message = [[SKPSMTPMessage alloc] init];
+		
+	message.fromEmail = from;
+	message.toEmail = to;
+	if (bcc == (id)[NSNull null] || bcc.length == 0 ){
+		message.bccEmail = nil;
+	} else {
+		message.bccEmail = bcc;		
+	}
+
+	message.relayHost = smtpServer;
+	//message.requiresAuth = [json objectForKey:@"smtpRequiresAuth"];
+	message.requiresAuth = true;
+		
+	if (message.requiresAuth) {
+		message.login = smtpUser;
+		message.pass = smtpPassword;
+	}
+		
+	message.wantsSecure = true; // smtp.gmail.com doesn't work without TLS!
+		
+	message.subject = [json objectForKey:@"subject"];
+		
+	// Only do this for self-signed certs, test only
+	// testMsg.validateSSLChain = NO;
+
+	message.delegate = self;
+		
+	NSDictionary *plainPart = [
+  	NSDictionary dictionaryWithObjectsAndKeys:@"text/plain; charset=UTF-8",
+    kSKPSMTPPartContentTypeKey,
+		textBody, kSKPSMTPPartMessageKey, @"8bit", 
+    kSKPSMTPPartContentTransferEncodingKey, nil
+  ];
+		
+	NSMutableArray *partsToSend = [NSMutableArray arrayWithObjects:plainPart, nil];
+		
+	NSArray *files = [json objectForKey:@"attachmentsInBase64Format"];
+
+	NSError *error = nil;
 	
-		   	NSTextCheckingResult *match = [regex firstMatchInString:file
-		                                     options:0
-		                                     range:NSMakeRange(0, [file length])];
-        
-		   	NSMutableString *fileName = [NSMutableString stringWithString:@"attachment."];
-		   	if (match) {
-   			    [fileName appendString:[file substringWithRange:[match rangeAtIndex:1]]];
-		   	}
-		   	else{
-   			    [fileName appendString:@"jpg"];
-		   	}
-        
-		   	NSMutableString *attachedFilename = [NSMutableString stringWithString:@"text/directory;\r\n\tx-unix-mode=0644;\r\n\tname=\""] ;
-		   	[attachedFilename appendString:fileName];
-        
-       	NSData *fileData = [[NSData alloc] initWithBase64EncodedString:[imageDataSplitByComma objectAtIndex:1] options:0];
-        
-		   	NSMutableString *attachementString = [NSMutableString stringWithString:@"attachment;\r\n\tfilename=\""];
-		   	[attachementString appendString:fileName];
-		   	[attachementString appendString:@"\""];
+	NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:@"\/(.*)\;" options:NSRegularExpressionCaseInsensitive error:&error];
 
-        
-		   	NSDictionary *filePart = [NSDictionary dictionaryWithObjectsAndKeys:attachedFilename,        kSKPSMTPPartContentTypeKey,
-		       	                      attachementString,kSKPSMTPPartContentDispositionKey,[fileData encodeBase64ForData],kSKPSMTPPartMessageKey,@"base64",kSKPSMTPPartContentTransferEncodingKey,nil];
-        
-		   	[partsToSend addObject:filePart];
-    }
-    
-    message.parts = partsToSend;
+		
+	for (NSString *file in files) {
 
-    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-       	[message send];
-    });
+		NSArray* imageDataSplitByComma = [file componentsSeparatedByString:@","];
+	
+		NSTextCheckingResult *match = [
+			regex firstMatchInString:file
+			options:0
+			range:NSMakeRange(0, [file length])
+    ];
+				
+		NSMutableString *fileName = [NSMutableString stringWithString:@"attachment."];
+		if (match) {
+			[fileName appendString:[file substringWithRange:[match rangeAtIndex:1]]];
+		} else {
+			[fileName appendString:@"jpg"];
+		}
+				
+		NSMutableString *attachedFilename = [
+      NSMutableString stringWithString:@"text/directory;\r\n\tx-unix-mode=0644;\r\n\tname=\""
+    ];
+		[attachedFilename appendString:fileName];
+				
+		NSData *fileData = [[NSData alloc] initWithBase64EncodedString:[imageDataSplitByComma objectAtIndex:1] options:0];
+				
+		NSMutableString *attachementString = [NSMutableString stringWithString:@"attachment;\r\n\tfilename=\""];
+		[attachementString appendString:fileName];
+		[attachementString appendString:@"\""];
+				
+		NSDictionary *filePart = [
+    	NSDictionary dictionaryWithObjectsAndKeys:attachedFilename,
+      kSKPSMTPPartContentTypeKey,
+			attachementString, kSKPSMTPPartContentDispositionKey,
+      [fileData encodeBase64ForData], kSKPSMTPPartMessageKey, @"base64",
+      kSKPSMTPPartContentTransferEncodingKey, nil
+    ];
+				
+		[partsToSend addObject:filePart];
+	}
+		
+	message.parts = partsToSend;
+
+	dispatch_async(dispatch_get_global_queue(
+  	DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			[message send];
+		}
+  );
 }
 
 - (void)messageSent:(SKPSMTPMessage *)message
 {
-    //NSLog(@"delegate - message sent");
+	//NSLog(@"delegate - message sent");
 	
-    NSDictionary *jsonObj = [
-   	   	[NSDictionary alloc]
-   	   	initWithObjectsAndKeys :
-   	   	@"", @"errorMessage",
-   	   	@"true", @"success",
-		   	@"0", @"errorCode",
-        nil 
-    ];
+	NSDictionary *jsonObj = [
+		[NSDictionary alloc] initWithObjectsAndKeys :@"", 
+    @"errorMessage", @"true", @"success",
+		@"0", @"errorCode", nil 
+	];
 	
-   	CDVPluginResult *pluginResult = [ 
-       	CDVPluginResult
-   	   	resultWithStatus    : CDVCommandStatus_OK
-   	   	messageAsString : jsonObj
-   	];
+	CDVPluginResult *pluginResult = [ 
+		CDVPluginResult resultWithStatus: CDVCommandStatus_OK messageAsString: jsonObj
+	];
 	
 	[self.commandDelegate sendPluginResult:pluginResult callbackId:self.pluginCallbackId];
 }
 
 - (void)messageFailed:(SKPSMTPMessage *)message error:(NSError *)error
 {
-    
-    NSString* errorMessage = [NSString stringWithFormat:@"Darn! Error!\n%i: %@\n%@", [error code], [error localizedDescription], [error localizedRecoverySuggestion]];
-    
-    
-   	NSDictionary *jsonObj = [ 
-   	   	[NSDictionary alloc]
-   	   	initWithObjectsAndKeys :
-   	   	errorMessage , @"errorMessage",
-   	   	@"false", @"success",
-   	   	[error code],@"errorCode",
-   	   	nil
-   	];
-   	CDVPluginResult *pluginResult = [
-   	   	CDVPluginResult
-   	   	resultWithStatus : CDVCommandStatus_OK
-   	   	messageAsString : jsonObj
-   	];			  
+		
+	NSString* errorMessage = [
+  	NSString stringWithFormat:@"Darn! Error!\n%i: %@\n%@", 
+  	[error code], [error localizedDescription], 
+    [error localizedRecoverySuggestion]
+  ];		
+		
+	NSDictionary *jsonObj = [ 
+		[NSDictionary alloc]
+		initWithObjectsAndKeys :
+		errorMessage , @"errorMessage",
+		@"false", @"success",
+		[error code],@"errorCode",
+		nil
+	];
+	CDVPluginResult *pluginResult = [
+		CDVPluginResult
+		resultWithStatus : CDVCommandStatus_OK
+		messageAsString : jsonObj
+	];				
 	
-   	[self.commandDelegate sendPluginResult:pluginResult callbackId:self.pluginCallbackId];
+	[self.commandDelegate sendPluginResult:pluginResult callbackId:self.pluginCallbackId];
 	
-    //self.textView.text = [NSString stringWithFormat:@"Darn! Error: %@, %@", [error code], [error localizedDescription]];
-    //self.textView.text = [NSString stringWithFormat:@"Darn! Error!\n%i: %@\n%@", [error code], [error localizedDescription], [error localizedRecoverySuggestion]];
-    //[message release];
-    
-    //NSLog(@"delegate - error(%d): %@", [error code], [error localizedDescription]);
+	//self.textView.text = [NSString stringWithFormat:@"Darn! Error: %@, %@", [error code], [error localizedDescription]];
+	//self.textView.text = [NSString stringWithFormat:@"Darn! Error!\n%i: %@\n%@", [error code], [error localizedDescription], [error localizedRecoverySuggestion]];
+	//[message release];
+		
+	//NSLog(@"delegate - error(%d): %@", [error code], [error localizedDescription]);
 }
 
 @end
--- src/ios/SMTPLibrary/Base64Transcoder.m
+++ src/ios/SMTPLibrary/Base64Transcoder.m
@@ -103,128 +103,113 @@ const UInt8 kBits_11111100 = 0xFC;
 
 size_t EstimateBas64EncodedDataSize(size_t inDataSize)
 {
-size_t theEncodedDataSize = (int)ceil(inDataSize / 3.0) * 4;
-theEncodedDataSize = theEncodedDataSize / 72 * 74 + theEncodedDataSize % 72;
+	size_t theEncodedDataSize = (int)ceil(inDataSize / 3.0) * 4;
+	theEncodedDataSize = theEncodedDataSize / 72 * 74 + theEncodedDataSize % 72;	
 return(theEncodedDataSize);
 }
 
 size_t EstimateBas64DecodedDataSize(size_t inDataSize)
 {
-size_t theDecodedDataSize = (int)ceil(inDataSize / 4.0) * 3;
-//theDecodedDataSize = theDecodedDataSize / 72 * 74 + theDecodedDataSize % 72;
-return(theDecodedDataSize);
+	size_t theDecodedDataSize = (int)ceil(inDataSize / 4.0) * 3;
+	//theDecodedDataSize = theDecodedDataSize / 72 * 74 + theDecodedDataSize % 72;
+	return(theDecodedDataSize);
 }
 
 bool Base64EncodeData(const void *inInputData, size_t inInputDataSize, char *outOutputData, size_t *ioOutputDataSize, BOOL wrapped)
 {
-size_t theEncodedDataSize = EstimateBas64EncodedDataSize(inInputDataSize);
-if (*ioOutputDataSize < theEncodedDataSize)
-	return(false);
-*ioOutputDataSize = theEncodedDataSize;
-const UInt8 *theInPtr = (const UInt8 *)inInputData;
-UInt32 theInIndex = 0, theOutIndex = 0;
-for (; theInIndex < (inInputDataSize / 3) * 3; theInIndex += 3)
-	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (theInPtr[theInIndex + 2] & kBits_11000000) >> 6];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 2] & kBits_00111111) >> 0];
-	if (wrapped && (theOutIndex % 74 == 72))
-		{
-		outOutputData[theOutIndex++] = '\r';
-		outOutputData[theOutIndex++] = '\n';
+	size_t theEncodedDataSize = EstimateBas64EncodedDataSize(inInputDataSize);
+	if (*ioOutputDataSize < theEncodedDataSize)
+		return(false);
+	*ioOutputDataSize = theEncodedDataSize;
+	const UInt8 *theInPtr = (const UInt8 *)inInputData;
+	UInt32 theInIndex = 0, theOutIndex = 0;
+	for (; theInIndex < (inInputDataSize / 3) * 3; theInIndex += 3) {
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (theInPtr[theInIndex + 2] & kBits_11000000) >> 6];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 2] & kBits_00111111) >> 0];
+		if (wrapped && (theOutIndex % 74 == 72)) {
+			outOutputData[theOutIndex++] = '\r';
+			outOutputData[theOutIndex++] = '\n';
 		}
 	}
-const size_t theRemainingBytes = inInputDataSize - theInIndex;
-if (theRemainingBytes == 1)
-	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (0 & kBits_11110000) >> 4];
-	outOutputData[theOutIndex++] = '=';
-	outOutputData[theOutIndex++] = '=';
-	if (wrapped && (theOutIndex % 74 == 72))
-		{
-		outOutputData[theOutIndex++] = '\r';
-		outOutputData[theOutIndex++] = '\n';
+	const size_t theRemainingBytes = inInputDataSize - theInIndex;
+	if (theRemainingBytes == 1) {
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (0 & kBits_11110000) >> 4];
+		outOutputData[theOutIndex++] = '=';
+		outOutputData[theOutIndex++] = '=';
+		if (wrapped && (theOutIndex % 74 == 72)) {
+			outOutputData[theOutIndex++] = '\r';
+			outOutputData[theOutIndex++] = '\n';
 		}
 	}
-else if (theRemainingBytes == 2)
-	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (0 & kBits_11000000) >> 6];
-	outOutputData[theOutIndex++] = '=';
-	if (wrapped && (theOutIndex % 74 == 72))
-		{
-		outOutputData[theOutIndex++] = '\r';
-		outOutputData[theOutIndex++] = '\n';
+	else if (theRemainingBytes == 2) {
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
+		outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (0 & kBits_11000000) >> 6];
+		outOutputData[theOutIndex++] = '=';
+		if (wrapped && (theOutIndex % 74 == 72)) {
+			outOutputData[theOutIndex++] = '\r';
+			outOutputData[theOutIndex++] = '\n';
 		}
 	}
-return(true);
+	return(true);
 }
 
 bool Base64DecodeData(const void *inInputData, size_t inInputDataSize, void *ioOutputData, size_t *ioOutputDataSize)
 {
-memset(ioOutputData, '.', *ioOutputDataSize);
+	memset(ioOutputData, '.', *ioOutputDataSize);
 
-size_t theDecodedDataSize = EstimateBas64DecodedDataSize(inInputDataSize);
-if (*ioOutputDataSize < theDecodedDataSize)
-	return(false);
-*ioOutputDataSize = 0;
-const UInt8 *theInPtr = (const UInt8 *)inInputData;
-UInt8 *theOutPtr = (UInt8 *)ioOutputData;
-size_t theInIndex = 0, theOutIndex = 0;
-UInt8 theOutputOctet;
-size_t theSequence = 0;
-for (; theInIndex < inInputDataSize; )
-	{
-	SInt8 theSextet = 0;
+	size_t theDecodedDataSize = EstimateBas64DecodedDataSize(inInputDataSize);
+	if (*ioOutputDataSize < theDecodedDataSize)
+		return(false);
+	*ioOutputDataSize = 0;
+	const UInt8 *theInPtr = (const UInt8 *)inInputData;
+	UInt8 *theOutPtr = (UInt8 *)ioOutputData;
+	size_t theInIndex = 0, theOutIndex = 0;
+	UInt8 theOutputOctet;
+	size_t theSequence = 0;
+	for (; theInIndex < inInputDataSize; ) {
+		SInt8 theSextet = 0;
 	
-	SInt8 theCurrentInputOctet = theInPtr[theInIndex];
-	theSextet = kBase64DecodeTable[theCurrentInputOctet];
-	if (theSextet == -1)
-		break;
-	while (theSextet == -2)
-		{
-		theCurrentInputOctet = theInPtr[++theInIndex];
+		SInt8 theCurrentInputOctet = theInPtr[theInIndex];
 		theSextet = kBase64DecodeTable[theCurrentInputOctet];
+		if (theSextet == -1)
+			break;
+		while (theSextet == -2) {
+			theCurrentInputOctet = theInPtr[++theInIndex];
+			theSextet = kBase64DecodeTable[theCurrentInputOctet];
 		}
-	while (theSextet == -3)
-		{
-		theCurrentInputOctet = theInPtr[++theInIndex];
-		theSextet = kBase64DecodeTable[theCurrentInputOctet];
+		while (theSextet == -3) {
+			theCurrentInputOctet = theInPtr[++theInIndex];
+			theSextet = kBase64DecodeTable[theCurrentInputOctet];
 		}
-	if (theSequence == 0)
-		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 2 & kBits_11111100;
+		if (theSequence == 0) {
+			theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 2 & kBits_11111100;
 		}
-	else if (theSequence == 1)
-		{
-		theOutputOctet |= (theSextet >- 0 ? theSextet : 0) >> 4 & kBits_00000011;
-		theOutPtr[theOutIndex++] = theOutputOctet;
+	  else if (theSequence == 1) {
+			theOutputOctet |= (theSextet >- 0 ? theSextet : 0) >> 4 & kBits_00000011;
+			theOutPtr[theOutIndex++] = theOutputOctet;
 		}
-	else if (theSequence == 2)
-		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 4 & kBits_11110000;
+		else if (theSequence == 2) {
+			theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 4 & kBits_11110000;
 		}
-	else if (theSequence == 3)
-		{
-		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 2 & kBits_00001111;
-		theOutPtr[theOutIndex++] = theOutputOctet;
+		else if (theSequence == 3) {
+			theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 2 & kBits_00001111;
+			theOutPtr[theOutIndex++] = theOutputOctet;
 		}
-	else if (theSequence == 4)
-		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 6 & kBits_11000000;
+		else if (theSequence == 4) {
+			theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 6 & kBits_11000000;
 		}
-	else if (theSequence == 5)
-		{
-		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 0 & kBits_00111111;
-		theOutPtr[theOutIndex++] = theOutputOctet;
+		else if (theSequence == 5) {
+			theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 0 & kBits_00111111;
+			theOutPtr[theOutIndex++] = theOutputOctet;
 		}
-	theSequence = (theSequence + 1) % 6;
-	if (theSequence != 2 && theSequence != 4)
-		theInIndex++;
+		theSequence = (theSequence + 1) % 6;
+		if (theSequence != 2 && theSequence != 4)
+			theInIndex++;
 	}
-*ioOutputDataSize = theOutIndex;
-return(true);
+	*ioOutputDataSize = theOutIndex;
+	return(true);
 }
