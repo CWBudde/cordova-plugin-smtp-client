8e282b84b6dda9c9e0b23409876eabc1c8444345
Thierry Poinot <poinot.thierry@gmail.com>
2019-05-13 14:43:55 +0200

    Update smtpClient.js

--- www/smtpClient.js
+++ www/smtpClient.js
@@ -3,6 +3,10 @@
 var smtpClient = {
     sendMail: function (mailSettings, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "SMTPClient", "cordovaSendMail", [ JSON.stringify(mailSettings) ]);
+    },
+    isLoaded: function() {
+        console.info('SMTP Client is loaded !');
+        return true;
     }
 };
 
